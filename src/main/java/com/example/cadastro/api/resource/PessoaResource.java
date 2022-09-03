4b9f40a24ba471a8f81a26b320a7718006c6598e
brunomartins32 <brunomartinns@hotmail.com>
2022-09-03 19:51:23 -0300

    Implementação do métodua de atualização (PUT)

--- src/main/java/com/example/cadastro/api/resource/PessoaResource.java
+++ src/main/java/com/example/cadastro/api/resource/PessoaResource.java
@@ -7,12 +7,14 @@ import java.util.Optional;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,8 +33,7 @@ public class PessoaResource {
 	public ResponseEntity<Pessoa> cadastrar(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		.buildAndExpand(pessoaSalva.getCodigo()).toUri();
 		return ResponseEntity.created(uri).body(pessoaSalva);
 	}
 
@@ -53,5 +54,11 @@ public class PessoaResource {
 		pessoaRepository.deleteById(codigo);
 	}
 	
+	@PutMapping("/{codigo}")
+	public Pessoa atualizar(@PathVariable int codigo, @Valid @RequestBody Pessoa pessoa){
+		Pessoa pessoaSalva = pessoaRepository.findById(codigo).get();
+		BeanUtils.copyProperties(pessoa, pessoaSalva, "codigo");
+		return pessoaRepository.save(pessoaSalva);
+	}
 	
 }
