d92ca000d2c53fd64d778ce4a80891a1c9cf9412
Victor Caselli <vd.caselli@gmail.com>
2022-09-14 16:37:46 -0300

    Melhorias e sugestões #2

--- src/main/java/com/example/cadastro/api/controller/PessoaController.java
+++ src/main/java/com/example/cadastro/api/controller/PessoaController.java
@@ -17,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.cadastro.api.dto.PessoaDTO;
 import com.example.cadastro.api.service.PessoaService;
 
-//TODO - Mudar o status do ResponseEntity, pode ser ok() a não ser que seja um SAVE, ai seria create
+//TODO - Mudar o status do ResponseEntity, pode ser ok() a não ser que seja um SAVE, ai seria created
 //TODO - Mudar os tipos do ResponseEntity
-//TODO - Mudar a injeção de dependência
+//TODO - Mudar a injeção de dependência para constructor e não usando @Autowired
 
 @RestController
 @RequestMapping("/pessoa")
@@ -42,11 +42,26 @@ public class PessoaController {
 		return ResponseEntity.ok(pessoaService.findById(codigo));
 	}
 
+
+	//Abaixo eu fiz um deleteById com o mesmo objetivo desse cara aqui
+	//Esse cara aqui não está incorreto, só mostrei uma forma alternativa usando ResponseEntity
 	@DeleteMapping("/{codigo}")
 	public void remover(@PathVariable Long codigo) {
 		pessoaService.deleteById(codigo);
 	}
 
+
+
+	@DeleteMapping("/alternativo/{id}")
+	public ResponseEntity<Void> deleteById(@PathVariable long id){
+		pessoaService.deleteById(id);
+
+		//Esse cara faz com que não tenha body na sua requisição, então é bom utilizar para retorno VOID
+		return ResponseEntity.noContent().build();
+	}
+
+
+	//TODO - Update não utiliza HTTP 201 ou seja, HTTP STATUS CREATED, mudar para http 200
 	@PutMapping("/{codigo}")
 	public ResponseEntity<PessoaDTO> atualizar(@PathVariable Long codigo, @RequestBody PessoaDTO pessoadto) {
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaService.atualizar(codigo, pessoadto));
